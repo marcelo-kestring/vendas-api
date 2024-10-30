45def4a43de66632c1427f07f869d63837f20702
Marcelo Kestring <marcelo.kestring@outlook.com>
2024-10-30 15:48:58 -0300

    feat(vendas): adicionar entidades de domínio, serviço e interfaces de repositório e serviço

---
+++ VendasAPISolution/VendasAPIBackend/Backend/Vendas.Dominio/Entities/Venda.cs
@@ -0,0 +1,10 @@
+﻿namespace Vendas.Dominio.Entities
+{
+    public class Venda
+    {
+        public int Id { get; set; }
+        public string NumeroVenda { get; set; }
+        public List<ItemVenda> Itens { get; set; }
+        public bool Cancelada { get; set; }
+    }
+}
