0ce55157c683a27560b56dbd3d835d27fcace7a8
Marcelo Kestring <marcelo.kestring@outlook.com>
2024-10-30 15:49:59 -0300

    feat(vendas): implementar repositório de vendas usando Entity Framework Core
    Adição do repositório de vendas usando Entity Framework Core (em memória).

---
+++ VendasAPISolution/VendasAPIBackend/Backend/Vendas.Infraestrutura/Repositories/VendaRepository.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Vendas.Dominio.Entities;
+using Vendas.Dominio.Repositories;
+using Vendas.Infraestrutura.Data;
+
+namespace Vendas.Infraestrutura.Repositories
+{
+    public class VendaRepository : IVendaRepository
+    {
+        private readonly ApplicationDbContext _context;
+
+        public VendaRepository(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IEnumerable<Venda>> ObterTodasVendasAsync()
+        {
+            return await _context.Vendas.ToListAsync();
+        }
+
+        public async Task<Venda> ObterVendaPorIdAsync(int id)
+        {
+            return await _context.Vendas.FindAsync(id);
+        }
+
+        public async Task CriarVendaAsync(Venda venda)
+        {
+            await _context.Vendas.AddAsync(venda);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task AtualizarVendaAsync(Venda venda)
+        {
+            _context.Vendas.Update(venda);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task DeletarVendaAsync(int id)
+        {
+            var venda = await ObterVendaPorIdAsync(id);
+            if (venda != null)
+            {
+                _context.Vendas.Remove(venda);
+                await _context.SaveChangesAsync();
+            }
+        }
+    }
+}
